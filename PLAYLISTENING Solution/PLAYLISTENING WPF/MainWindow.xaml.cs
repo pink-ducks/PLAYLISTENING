a22a766f2f6e9974dbd55067261cc08c8d1a214e
Mateusz <matpiwowarski7@gmail.com>
2019-08-22 21:45:54 +0200

    Try to connect with Spotify API

--- PLAYLISTENING Solution/PLAYLISTENING WPF/MainWindow.xaml.cs
+++ PLAYLISTENING Solution/PLAYLISTENING WPF/MainWindow.xaml.cs
@@ -13,38 +13,81 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using SpotifyAPI.Web;
+using SpotifyAPI.Web.Auth;
 using SpotifyAPI.Web.Enums;
 using SpotifyAPI.Web.Models;
+// 2726b49d5bf540e2ab307852eb45a438 // clientID
+// 5b453a702f944aaeb40d23077aea2d16 // secretID
 
 namespace PLAYLISTENING_WPF
 {
     public partial class MainWindow : Window
     {
-        public static async void Example()
-        {
-            SpotifyWebAPI api = new SpotifyWebAPI
-            {
-                AccessToken = "XX?X?X",
-                TokenType = "Bearer"
-            };
-
-            PrivateProfile profile = await api.GetPrivateProfileAsync();
-            if (!profile.HasError())
-            {
-                Console.WriteLine(profile.DisplayName);
-
-            }
-            else
-            {
-                Console.WriteLine(profile.Error.Message);
-                Console.WriteLine(profile.Error.Status);
-            }
-        }
+        private static string _clientId = ""; //"";
+        private static string _secretId = ""; //"";
 
         public MainWindow()
         {
-            Example();
-            InitializeComponent();
+            _clientId = string.IsNullOrEmpty(_clientId)
+    ? Environment.GetEnvironmentVariable("SPOTIFY_CLIENT_ID")
+    : _clientId;
+
+            _secretId = string.IsNullOrEmpty(_secretId)
+                ? Environment.GetEnvironmentVariable("SPOTIFY_SECRET_ID")
+                : _secretId;
+
+            Console.WriteLine("####### Spotify API Example #######");
+            Console.WriteLine("This example uses AuthorizationCodeAuth.");
+            Console.WriteLine(
+                "Tip: If you want to supply your ClientID and SecretId beforehand, use env variables (SPOTIFY_CLIENT_ID and SPOTIFY_SECRET_ID)");
+
+            AuthorizationCodeAuth auth =
+                new AuthorizationCodeAuth(_clientId, _secretId, "http://localhost:8888/callback", "http://localhost:8888/callback",
+                    Scope.PlaylistReadPrivate | Scope.PlaylistReadCollaborative);
+            auth.AuthReceived += AuthOnAuthReceived;
+            auth.Start();
+            auth.OpenBrowser();
+
+            Console.ReadLine();
+            auth.Stop(0);
+
+            InitializeComponent(); // DO NOT DELETE THIS
+        }
+
+        private static async void AuthOnAuthReceived(object sender, AuthorizationCode payload)
+        {
+            AuthorizationCodeAuth auth = (AuthorizationCodeAuth)sender;
+            auth.Stop();
+
+
+
+            Token token = await auth.ExchangeCode(payload.Code);
+            SpotifyWebAPI api = new SpotifyWebAPI
+            {
+                AccessToken = token.AccessToken,
+                TokenType = token.TokenType
+            };
+            PrintUsefulData(api);
+        }
+
+        private static async void PrintUsefulData(SpotifyWebAPI api)
+        {
+            PrivateProfile profile = await api.GetPrivateProfileAsync();
+            string name = string.IsNullOrEmpty(profile.DisplayName) ? profile.Id : profile.DisplayName;
+            Console.WriteLine($"Hello there, {name}!");
+
+            Console.WriteLine("Your playlists:");
+            Paging<SimplePlaylist> playlists = await api.GetUserPlaylistsAsync(profile.Id);
+            do
+            {
+                playlists.Items.ForEach(playlist =>
+                {
+                    Console.WriteLine($"- {playlist.Name}");
+                });
+                playlists = await api.GetNextPageAsync(playlists);
+            } while (playlists.HasNextPage());
+
+
         }
     }
 }
