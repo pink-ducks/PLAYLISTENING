7045c16082dca5c0508b0d44874f2bfeb5106ab9
romanzemela <roman.zemela@gmail.com>
2019-08-26 22:03:24 +0200

    playlist and track model
    Added Models for track and playlist

---
+++ PLAYLISTENING Solution/PLAYLISTENING WPF/Models/Playlist.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PLAYLISTENING_WPF.Models
+{
+    class Playlist
+    {
+        private string id;
+        private string name;
+        private string imageURL;
+        private string ownerID;
+        private ArrayList tracksIDs;
+
+        public string Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public string ImageURL { get => imageURL; set => imageURL = value; }
+        public string OwnerID { get => ownerID; set => ownerID = value; }
+        public ArrayList TracksIDs { get => tracksIDs; set => tracksIDs = value; }
+    }
+}
---
+++ PLAYLISTENING Solution/PLAYLISTENING WPF/Models/Track.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PLAYLISTENING_WPF.Models
+{
+    class Track
+    {
+        private string id;
+        private string name;
+        private string imageURL;
+        private string authorID;
+        private string lenght;
+
+        public string Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public string ImageURL { get => imageURL; set => imageURL = value; }
+        public string AuthorID { get => authorID; set => authorID = value; }
+        public string Lenght { get => lenght; set => lenght = value; }
+    }
+}
